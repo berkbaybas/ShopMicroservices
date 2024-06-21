26f8be6f109ba0277aed2e88eca3e38dbec5a4c0
berk <berk_baybas@hotmail.com>
2024-06-21 16:01:43 +0300

    Consume discount grpc from basket api when add item to shopping cart

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -2,6 +2,7 @@ using Basket.API.Data;
 using BuildingBlocks.Exceptions.Handler;
 using BuildingBlocks.Extentions;
 using BuildingBlocks.PipelineBehaviors;
+using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
@@ -11,6 +12,7 @@ var assembly = typeof(Program).Assembly;
 var connectionStringPostgre = builder.Configuration.GetConnectionString("Database")!;
 var connectionStringRedis = builder.Configuration.GetConnectionString("Database")!;
 
+//Application Services
 builder.Services.AddCarterWithAssemblies(assembly);
 
 builder.Services.AddMediatR(config =>
@@ -20,21 +22,40 @@ builder.Services.AddMediatR(config =>
     config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+//Data Services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(connectionStringPostgre);
 }).UseLightweightSessions();
-
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
-builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
 
 builder.Services.AddStackExchangeRedisCache(opt =>
 {
     opt.Configuration = builder.Configuration.GetConnectionString("Redis");
 });
 
+//Grpc Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
+});
+
+
+//Cross-Cutting Services
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 builder.Services.AddHealthChecks()
                     .AddNpgSql(connectionStringPostgre)
                     .AddRedis(connectionStringRedis);
